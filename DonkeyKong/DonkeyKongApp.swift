f653e6a9a9689dc19bc32141c9010e47f62b24eb
Soham Gupta <guptasoham@outlook.com>
2022-01-18 20:02:27 -0600

    Initial Commit

---
+++ DonkeyKong/DonkeyKongApp.swift
@@ -0,0 +1,17 @@
+//
+//  DonkeyKongApp.swift
+//  DonkeyKong
+//
+//  Created by soham gupta on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct DonkeyKongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
